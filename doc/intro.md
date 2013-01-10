6f06a02097da1d8d82dde927dd332b7c6b7a2609
Anton Podviaznikov <podviaznikov@gmail.com>
2013-01-10 20:47:43 -0200

    intial upload of empty project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to msg-api-comp
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/msg_api_comp/core_test.clj
@@ -0,0 +1,7 @@
+(ns msg-api-comp.core-test
+  (:use clojure.test
+        msg-api-comp.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
