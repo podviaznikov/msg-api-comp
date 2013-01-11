5dfdeb33141f4b6072920e056f96e76397047b25
Anton Podviaznikov <podviaznikov@gmail.com>
2013-01-11 00:01:22 -0200

    experimenting with subqueries

--- src/msg_api_comp/services/msg_service.clj
+++ src/msg_api_comp/services/msg_service.clj
@@ -15,6 +15,13 @@
   (select messages
     (where {:topic_urn topic_urn})))
 
+(defn inbox [user_urn]
+	(select [messages :c1]
+        (fields :topic_urn 
+         	[(subselect messages
+               (aggregate (count :*) :message_count)
+               (where {:topic_urn :c1.topic_urn})) :message_count])))
+
 (defn create [message]
   (insert messages 
     (values message)))
@@ -25,4 +32,15 @@
            :sender_urn "http://twitter.com/podviaznikov"
            :receiver_urn "http://twitter.com/m_aleksandrova"
            :message "Hi, can we discuss this project together?"})
+  (create {:topic_urn "msg-api-comp discusiion"
+           :sender_urn "http://twitter.com/m_aleksandrova"
+           :receiver_urn "http://twitter.com/podviaznikov"
+           :message "Yeah, sure. When do you want to do it?"})
+  (create {:topic_urn "new year animation"
+           :sender_urn "http://twitter.com/m_aleksandrova"
+           :receiver_urn "http://twitter.com/anton"
+           :message "We need to discuss New Year animation project"})
+  (all-for-topic "msg-api-comp discusiion")
+
+  (all-for-topic "msg-api-comp discusiion")
   (println " done..."))
